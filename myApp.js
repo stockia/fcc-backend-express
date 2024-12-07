614cb4e47e0e6835541b2f0d22cd676277f09058
Alex Stocki <ladislao.stocki@gointegro.com>
2024-12-07 14:54:26 -0300

    feat: challenge 03/12 - Node and Express

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 function get(req, res) {
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 }
 
 app.get('/', get);
