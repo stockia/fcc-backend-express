421104d65c82303ee72161dad9fb786acf0e6198
Alex Stocki <ladislao.stocki@gointegro.com>
2024-12-07 17:58:08 -0300

    feat: challenge 04/12 - Node and Express

--- myApp.js
+++ myApp.js
@@ -2,10 +2,14 @@ let express = require('express');
 let app = express();
 
 const absolutePath = __dirname + '/views/index.html';
+const absolutePathStatic = __dirname + ''
+
+app.use('/public', express.static(absolutePathStatic));
 
 app.get('/', function(req, res) {
     res.sendFile(absolutePath);
 });
+
 
 
 
